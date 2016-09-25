45fd45937d684bf33b500b5162adb094a813c17e
Andrew Poyntz <andrewpoyntz@hotmail.com>
2016-09-25 18:24:37 +0100

    added some actually relevant tests

--- test/time-ago-pipe.spec.ts
+++ test/time-ago-pipe.spec.ts
@@ -1,10 +1,29 @@
 var expect = require('chai').expect;
+var sinon = require('sinon');
 var reflect = require('reflect-metadata');
 import {TimeAgoPipe} from "../time-ago-pipe";
+
 describe('time-ago-pipe', () => {
-	// This pipe is a pure function so no need for BeforeEach
-	let pipe = new TimeAgoPipe();
-	it('has a test', () => {
-		expect(true).to.equal(true);
+	const now:Date = new Date();
+	var clock:any;
+	beforeEach(() => {
+		clock = sinon.useFakeTimers(now.getTime());
 	});
+	afterEach(() => {
+		clock.restore();
+	});
+	let pipe = new TimeAgoPipe();
+	it('a few seconds ago', () => {
+		var pastDate = new Date();
+		for (let i =0; i < 50; i++){
+			//run the clock forward 1 secs
+			clock.tick(1000);
+			if (i <=44){ // less than 45 secs
+				expect(pipe.transform(pastDate.toString())).to.equal('a few seconds ago');
+			} else { // more than 45 secs ago
+				expect(pipe.transform(pastDate.toString())).to.equal('a minute ago');
+			}
+		}
+	});
+
 });
