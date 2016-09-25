258635c41468fb7713d4c278e462f6944716825c
Andrew Poyntz <andrewpoyntz@hotmail.com>
2016-09-25 16:43:30 +0100

    npm ignore added  d.ts now generated & referenced properly in the package.json

--- gulpfile.ts
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp');
-const typescript = require('gulp-typescript');
-
-gulp.task('compile', function(){
-	return gulp.src(['src/ts/**/*.ts'])
-		.pipe(typescript())
-		.pipe(gulp.dest('src/js/'));
-});
