16773e538045ea5cf82ce1125cec17c4e2e10d88
Andrew Poyntz <andrewpoyntz@hotmail.com>
2016-10-08 21:10:17 +0100

    feat(pipe): text value will now auto update as time passes
    The frequency of updates is based on how long ago the time was - i.e. if it's over an hour ago, not much point in checking every second!
    
     closes #1

--- test/time-ago-pipe.spec.ts
+++ test/time-ago-pipe.spec.ts
@@ -1,9 +1,18 @@
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var reflect = require('reflect-metadata');
+import {NgZone} from '@angular/core';
 import {TimeAgoPipe} from "../time-ago-pipe";
 
-// TODO:these don't cover all the edge cases, so need improving, luckily, the edges are very blurred with approximations, so you probably won't notice the difference!
+class NgZoneMock {
+	runOutsideAngular (fn: Function) {
+		return fn();
+	}
+	run(fn: Function) {
+		return fn();
+	}
+};
+
 describe('time-ago-pipe', () => {
 	const now:Date = new Date();
 	var clock:any;
@@ -18,134 +27,135 @@ describe('time-ago-pipe', () => {
 	afterEach(() => {
 		clock.restore();
 	});
-	let pipe = new TimeAgoPipe();
-	it('\'a few seconds ago\' tests', () => {
-		var pastDate = new Date();
-		for (let i =0; i < 45; i++){
-			clock.tick(oneSec);
-			if (i < 44) {
-				expect(pipe.transform(pastDate.toString())).to.equal('a few seconds ago');
+	describe('output tests', function () {
+		let pipe = new TimeAgoPipe(null, new NgZoneMock() as NgZone);
+		it('\'a few seconds ago\' tests', () => {
+			var pastDate = new Date();
+			for (let i =0; i < 45; i++){
+				clock.tick(oneSec);
+				if (i < 44) {
+					expect(pipe.transform(pastDate.toString())).to.equal('a few seconds ago');
+				}
 			}
-		}
-	});
-	it('\'a minute ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneSec * 45);
-		for (let i =45; i < 89; i++){
-			clock.tick(oneSec);
-			if (i < 89){
-				expect(pipe.transform(pastDate.toString())).to.equal('a minute ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal('a minute ago');
+		});
+		it('\'a minute ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneSec * 45);
+			for (let i =45; i < 89; i++){
+				clock.tick(oneSec);
+				if (i < 89){
+					expect(pipe.transform(pastDate.toString())).to.equal('a minute ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal('a minute ago');
+				}
 			}
-		}
-	});
-	it('\'x minutes ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneSec * 50);
-		for (let i =1; i < 44; i++){
-			clock.tick(oneMin);
-			if (i < 44){
-				expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' minutes ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' minutes ago');
+		});
+		it('\'x minutes ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneSec * 50);
+			for (let i =1; i < 44; i++){
+				clock.tick(oneMin);
+				if (i < 44){
+					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' minutes ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' minutes ago');
+				}
 			}
-		}
-	});
-	it('\'an hour ago\' tests', () => {
-		var pastDate = new Date();
-		//set the time forward 45 mins
-		clock.tick(oneMin * 45);
-		for (let i =45; i < 120; i++){
-			clock.tick(oneMin);
-			if (i < 90){
-				expect(pipe.transform(pastDate.toString())).to.equal('an hour ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal('an hour ago');
+		});
+		it('\'an hour ago\' tests', () => {
+			var pastDate = new Date();
+			//set the time forward 45 mins
+			clock.tick(oneMin * 45);
+			for (let i =45; i < 120; i++){
+				clock.tick(oneMin);
+				if (i < 90){
+					expect(pipe.transform(pastDate.toString())).to.equal('an hour ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal('an hour ago');
+				}
 			}
-		}
-	});
-	it('\'x hours ago\' tests', () => {
-		var pastDate = new Date();
-		//set the time forward 50 mins
-		clock.tick(oneMin * 50);
-		for (let i = 1; i < 25; i++){
-			clock.tick(oneHour);
-			if (i < 22){
-				expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' hours ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' hours ago');
+		});
+		it('\'x hours ago\' tests', () => {
+			var pastDate = new Date();
+			//set the time forward 50 mins
+			clock.tick(oneMin * 50);
+			for (let i = 1; i < 25; i++){
+				clock.tick(oneHour);
+				if (i < 22){
+					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' hours ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' hours ago');
+				}
 			}
-		}
-	});
-	it('\'a day ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneHour * 22);
-		for (let i = 22; i < 40; i++){
-			clock.tick(oneHour);
-			if (i < 36){
-				expect(pipe.transform(pastDate.toString())).to.equal('a day ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal('a day ago');
+		});
+		it('\'a day ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneHour * 22);
+			for (let i = 22; i < 40; i++){
+				clock.tick(oneHour);
+				if (i < 36){
+					expect(pipe.transform(pastDate.toString())).to.equal('a day ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal('a day ago');
+				}
 			}
-		}
-	});
-	it('\'x days ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneHour * 35);
-		for (let i = 1; i < 30; i++){
-			clock.tick(oneDay);
-			if (i < 25){
-				expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' days ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' days ago');
+		});
+		it('\'x days ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneHour * 35);
+			for (let i = 1; i < 30; i++){
+				clock.tick(oneDay);
+				if (i < 25){
+					expect(pipe.transform(pastDate.toString())).to.equal(i+1 + ' days ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i+1 + ' days ago');
+				}
 			}
-		}
-	});
-	it('\'a month ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneDay * 25);
-		for (let i = 25; i < 50; i++){
-			clock.tick(oneDay);
-			if (i < 45){
-				expect(pipe.transform(pastDate.toString())).to.equal('a month ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal('a month ago');
+		});
+		it('\'a month ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneDay * 25);
+			for (let i = 25; i < 50; i++){
+				clock.tick(oneDay);
+				if (i < 45){
+					expect(pipe.transform(pastDate.toString())).to.equal('a month ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal('a month ago');
+				}
 			}
-		}
-	});
-	it('\'x month ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneDay * 43);
-		for (let i = 1; i < 13; i++){
-			clock.tick(oneMonth);
-			if (i < 10){
-				expect(pipe.transform(pastDate.toString())).to.equal(i +1 + ' months ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal(i +1 + ' months ago');
+		});
+		it('\'x month ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneDay * 43);
+			for (let i = 1; i < 13; i++){
+				clock.tick(oneMonth);
+				if (i < 10){
+					expect(pipe.transform(pastDate.toString())).to.equal(i +1 + ' months ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal(i +1 + ' months ago');
+				}
 			}
-		}
-	});
-	it('\'a year ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneDay * 345);
-		for (let i = 345; i < 545; i++){
-			clock.tick(oneDay);
-			if (i < 545){
-				expect(pipe.transform(pastDate.toString())).to.equal('a year ago');
-			} else {
-				expect(pipe.transform(pastDate.toString())).not.to.equal('a year ago');
+		});
+		it('\'a year ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneDay * 345);
+			for (let i = 345; i < 545; i++){
+				clock.tick(oneDay);
+				if (i < 545){
+					expect(pipe.transform(pastDate.toString())).to.equal('a year ago');
+				} else {
+					expect(pipe.transform(pastDate.toString())).not.to.equal('a year ago');
+				}
 			}
-		}
+		});
+		it('\'a year ago\' tests', () => {
+			var pastDate = new Date();
+			clock.tick(oneMonth * 22);
+			expect(pipe.transform(pastDate.toString())).to.equal(2 + ' years ago');
+			clock.tick(oneMonth * 12);
+			expect(pipe.transform(pastDate.toString())).to.equal(3 + ' years ago');
+			clock.tick(oneMonth * 36);
+			expect(pipe.transform(pastDate.toString())).to.equal(6 + ' years ago');
+		});
 	});
-	it('\'a year ago\' tests', () => {
-		var pastDate = new Date();
-		clock.tick(oneMonth * 22);
-		expect(pipe.transform(pastDate.toString())).to.equal(2 + ' years ago');
-		clock.tick(oneMonth * 12);
-		expect(pipe.transform(pastDate.toString())).to.equal(3 + ' years ago');
-		clock.tick(oneMonth * 36);
-		expect(pipe.transform(pastDate.toString())).to.equal(6 + ' years ago');
-	});
-
 });
--- time-ago-pipe.ts
+++ time-ago-pipe.ts
@@ -1,13 +1,24 @@
-import {Pipe, PipeTransform} from "@angular/core";
+import {Pipe, PipeTransform, NgZone, ChangeDetectorRef, OnDestroy} from "@angular/core";
 @Pipe({
 	name:'timeAgo',
 	pure:false
 })
-export class TimeAgoPipe implements PipeTransform {
+export class TimeAgoPipe implements PipeTransform, OnDestroy {
+	private timer: number;
+	constructor(private changeDetectorRef: ChangeDetectorRef, private ngZone: NgZone) {}
 	transform(value:string) {
+		this.removeTimer();
 		let d = new Date(value);
 		let now = new Date();
 		let seconds = Math.round(Math.abs((now.getTime() - d.getTime())/1000));
+		let timeToUpdate = this.getSecondsUntilUpdate(seconds) *1000;
+		this.timer = this.ngZone.runOutsideAngular(() => {
+			if (typeof window !== 'undefined') {
+				return window.setTimeout(() => {
+					this.ngZone.run(() => this.changeDetectorRef.markForCheck());
+				}, timeToUpdate);
+			}
+		});
 		let minutes = Math.round(Math.abs(seconds / 60));
 		let hours = Math.round(Math.abs(minutes / 60));
 		let days = Math.round(Math.abs(hours / 24));
@@ -35,6 +46,29 @@ export class TimeAgoPipe implements PipeTransform {
 			return 'a year ago';
 		} else if (days > 546) {
 			return years + ' years ago';
+		}
+	}
+	ngOnDestroy(): void {
+		this.removeTimer();
+	}
+	private removeTimer() {
+		if (this.timer) {
+			window.clearTimeout(this.timer);
+			this.timer = null;
+		}
+	}
+	private getSecondsUntilUpdate(seconds:number) {
+		let min = 60;
+		let hr = min * 60;
+		let day = hr * 24;
+		if (seconds < min) { // less than 1 min, update ever 2 secs
+			return 2;
+		} else if (seconds < hr) { // less than an hour, update every 30 secs
+			return 30;
+		} else if (seconds < day) { // less then a day, update every 5 mins
+			return 300;
+		} else { // update every hour
+			return 3600;
 		}
 	}
 }
